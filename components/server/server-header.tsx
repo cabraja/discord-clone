faa53f021bfb34031243358dbc170a7194e79109
cabraja <cabrajamihajlo@gmail.com>
2023-10-11 21:41:23 +0200

    add server edit modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -39,6 +39,7 @@ const ServerHeader = ({server,role}:ServerHeaderProps) => {
 
             {isAdmin && (
                 <DropdownMenuItem
+                onClick={() => onOpen('editServer',{server:server})}
                 className="px-3 py-2 text-sm cursor-pointer"
                 >
                     Server Settings<Settings className="h-4 w-4 ml-auto"/>
