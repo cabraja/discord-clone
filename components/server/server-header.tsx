46b58f56ef1d5af46620d4826dff05826443faee
cabraja <cabrajamihajlo@gmail.com>
2023-10-12 22:41:32 +0200

    add members modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -48,6 +48,7 @@ const ServerHeader = ({server,role}:ServerHeaderProps) => {
 
             {isAdmin && (
                 <DropdownMenuItem
+                onClick={() => onOpen('members',{server:server})}
                 className="px-3 py-2 text-sm cursor-pointer"
                 >
                     Manage Members<Users className="h-4 w-4 ml-auto"/>
