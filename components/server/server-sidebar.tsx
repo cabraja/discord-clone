18c8c065afbeebcdbce10f0a40918fa2e630de8a
cabraja <cabrajamihajlo@gmail.com>
2023-11-19 16:02:59 +0100

    add search dialog

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,12 +1,28 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirectToSignIn } from "@clerk/nextjs";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import db from '@/lib/db'
 import { redirect } from "next/navigation";
 import ServerHeader from "./server-header";
+import { ScrollArea } from "../ui/scroll-area";
+import ServerSearch from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { channel } from "diagnostics_channel";
 
 type ServerSidebarProps = {
     serverId:string;
+}
+
+const iconMap = {
+    [ChannelType.TEXT]:<Hash className="mr-2 h-4 w-4"/>,
+    [ChannelType.AUDIO]:<Mic className="mr-2 h-4 w-4"/>,
+    [ChannelType.VIDEO]:<Video className="mr-2 h-4 w-4"/>,
+}
+
+const roleIconMap = {
+    [MemberRole.GUEST]: null,
+    [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500"/>,
+    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>
 }
 
 const ServerSidebar = async({serverId}:ServerSidebarProps) => {
@@ -42,8 +58,8 @@ const ServerSidebar = async({serverId}:ServerSidebarProps) => {
     }
 
     const textChannels = server.channels.filter(channel => channel.type === ChannelType.TEXT)
-    const audioChannel = server.channels.filter(channel => channel.type === ChannelType.AUDIO)
-    const videoChannel = server.channels.filter(channel => channel.type === ChannelType.VIDEO)
+    const audioChannels = server.channels.filter(channel => channel.type === ChannelType.AUDIO)
+    const videoChannels = server.channels.filter(channel => channel.type === ChannelType.VIDEO)
     const members = server.members.filter(member => member.profileId !== profile.id)
 
     const role = server.members.find(member => member.profileId === profile.id)?.role;
@@ -51,6 +67,50 @@ const ServerSidebar = async({serverId}:ServerSidebarProps) => {
     return ( 
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
         <ServerHeader server={server} role={role}/>
+
+        <ScrollArea className="flex-1 px-3">
+            <div className="mt-2">
+                <ServerSearch data={[
+                    {
+                        label:'Text Channels',
+                        type: 'channel',
+                        data: textChannels.map(channel => ({
+                            id: channel.id,
+                            name: channel.name,
+                            icon: iconMap[channel.type]
+                        }))
+                    },
+                    {
+                        label:'Voice Channels',
+                        type: 'channel',
+                        data: audioChannels.map(channel => ({
+                            id: channel.id,
+                            name: channel.name,
+                            icon: iconMap[channel.type]
+                        }))
+                    },
+                    {
+                        label:'Video Channels',
+                        type: 'channel',
+                        data: videoChannels.map(channel => ({
+                            id: channel.id,
+                            name: channel.name,
+                            icon: iconMap[channel.type]
+                        }))
+                    },
+                    {
+                        label:'Members',
+                        type: 'member',
+                        data: members.map(member => ({
+                            id: member.id,
+                            name: member.profile.name,
+                            icon: roleIconMap[member.role]
+                        }))
+                    },
+                ]}/>
+            </div>
+
+        </ScrollArea>
     </div> );
 }
  
---
+++ lib/get-os.ts
@@ -0,0 +1,17 @@
+export default function getOS() {
+    let userAgent = window.navigator.userAgent.toLowerCase(),
+      macosPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i,
+      windowsPlatforms = /(win32|win64|windows|wince)/i,
+      iosPlatforms = /(iphone|ipad|ipod)/i,
+      os = null;
+  
+    if (macosPlatforms.test(userAgent)) {
+      os = "macos";
+    } else if (windowsPlatforms.test(userAgent)) {
+      os = "windows";
+    }else{
+        os = "other"
+    }
+  
+    return os;
+  }
