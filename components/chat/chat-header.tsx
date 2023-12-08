2c0d28ee4c3e6b41cbcf9f44ee3ee6022b09cd49
cabraja <cabrajamihajlo@gmail.com>
2023-12-08 01:05:27 +0100

    sokcet.io setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react"
 import MobileToggle from "../mobile-toggle";
 import UserAvatar from "../user-avatar";
+import SocketIndicator from "../socket-indicator";
 
 type ChatHeaderProps = {
     serverId:string;
@@ -24,6 +25,10 @@ function ChatHeader({serverId,name,type,imageUrl}:ChatHeaderProps) {
         <p className="font-semibold text-md text-black dark:text-white">
             {name}
         </p>
+
+        <div className="ml-auto flex items-center">
+            <SocketIndicator />
+        </div>
     </div>
   )
 }
